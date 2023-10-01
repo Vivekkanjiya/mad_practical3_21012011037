e048fe34f47970dbc0865256c5881d945eb74004
patelhir2010 <139534069+patelhir2010@users.noreply.github.com>
2023-10-01 11:06:00 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mad_practical3_21012011037"
+include(":app")
+ 
